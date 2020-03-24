a2317219532a304982c4b1dd5fa99d79fb4eba25
Rafael Sperandio Scheiner <rscheiner@ciandt.com>
2020-03-24 10:39:09 -0300

    Botao pronto para redirecionar para outra pagina, faltando a referencia apenas

--- prototipo-ui/telalogin/src/component/App.js
+++ prototipo-ui/telalogin/src/component/App.js
@@ -1,24 +1,30 @@
 import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <label 
-      className = "labelName"> Nome </label>
-      <div className= "divInputButton">
-        <div>
-          <input 
-            className = "inputText"
-            id="nameArea" 
-            rows = "1" 
-            cols="20"  
-            maxLength = "20" 
-            required></input>
+class loginLayout extends Component {
+
+  render(){
+    return (
+      <div className="App">
+        <label 
+        className = "labelName"> Nome </label>
+        <div className= "divInputButton">
+          <div>
+            <input 
+              className = "inputText"
+              id="nameArea" 
+              rows = "1" 
+              cols="20"  
+              maxLength = "20" 
+              required></input>
+          </div>
+            <button 
+              className = "login"
+              onClick ={event => window.location.href='/'}
+            >Entrar</button>
         </div>
-        <button className = "login">Entrar</button>
-        </div>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
-export default App;
+export default loginLayout;
