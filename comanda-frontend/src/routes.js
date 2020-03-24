18020380b30e9b307e0dea493d875128c5d88189
Danilo de Nadai Sicari <danilons@ciandt.com>
2020-03-24 14:58:29 -0300

    Adicionado prototipo login ao foundation

--- comanda-frontend/src/routes.js
+++ comanda-frontend/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Login from "./pages/Login";
 import Users from "./pages/User";
 import NewUser from "./pages/NewUser";
 
@@ -9,6 +10,7 @@ export default function Routes(){
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Users} />
+                <Route path="/login" component={Login} />
                 <Route path="/users" component={Users} />
                 <Route path="/newuser" component={NewUser} />
             </Switch>
