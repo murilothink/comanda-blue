860e8d41ba23b949338c5020445a3fa1a89140d2
Rafael Sperandio Scheiner <rscheiner@ciandt.com>
2020-03-24 13:59:13 -0300

    mudança de HTML e CSS para Material UI

--- prototipo-ui/telalogin/src/component/App.js
+++ prototipo-ui/telalogin/src/component/App.js
@@ -1,49 +1,70 @@
-import React,{Component} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
+import { Grid } from '@material-ui/core';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-    },
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    centerBlock: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
   },
+  
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+
 }));
 
-class loginLayout extends Component {
 
-  render(){
+export default function SignIn() {
+  const classes = useStyles();
+
     return (
-      <div className="App">
-        <label 
-        className = "labelName"> Nome </label>
-        <div className= "divInputButton">
-          <div>
-            <input 
-              className = "inputText"
-              id="nameArea" 
-              rows = "1" 
-              cols="20"  
-              maxLength = "20" 
-              required></input>
-          </div>
-            <button 
-              className = "login"
-              onClick ={event => window.location.href='/'}
-            >Entrar</button>
-            <p>
-            <Button variant="contained" color="primary">
-            Primary
-            </Button>
-            </p>
-        </div>
-      </div>
-    );
-  }
-}
+        <Container component="main" maxWidth = "xs" style={{height:"100%"}}>
+          <CssBaseline/>
+            
+            <Grid
+              container
+              style={{height:"100%"}}
+              spacing={0}
+              align="center"
+              justify="center"
+              direction="column"
+            >
 
-export default loginLayout;
+              <Typography component="h1" variant="h5">
+                Bem Vindo!
+              </Typography>
+
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="Nome"
+                name="name"
+                autoComplete="name"
+                autoFocus
+              />
+
+              <Button variant="contained" color="primary">
+                Entrar
+              </Button>
+
+            </Grid>
+            
+        </Container>
+    );
+}
