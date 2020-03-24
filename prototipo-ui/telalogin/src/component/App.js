9d8ce11ba40498536c679395161a0b42dd79dadc
Rafael Sperandio Scheiner <rscheiner@ciandt.com>
2020-03-24 10:09:40 -0300

    Tela Login + css feitos

--- prototipo-ui/telalogin/src/component/App.js
+++ prototipo-ui/telalogin/src/component/App.js
@@ -3,7 +3,22 @@ import React,{Component} from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Comanda</h1>
+    <ul>
+      <label 
+      className = "labelName"> Nome </label>
+      <div className= "divInputButton">
+        <div>
+          <input 
+            className = "inputText"
+            id="nameArea" 
+            rows = "1" 
+            cols="20"  
+            maxLength = "20" 
+            required></input>
+        </div>
+        <button className = "login">Entrar</button>
+        </div>
+      </ul>
     </div>
   );
 }
