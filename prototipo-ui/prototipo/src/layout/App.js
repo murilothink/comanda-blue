ee2e9cdb817252831d430c12369eb52fb5ca82b0
Renato Vinicius dos Santos <renatods@ciandt.com>
2020-03-25 11:36:57 -0300

    Layout 1.0

---
+++ prototipo-ui/prototipo/src/layout/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './base.css';
+import ComandaHeader from './ComandaHeader';
+import ComandaFooter from './ComandaFooter';
+
+class App extends Component {
+    render(){
+        return (
+          <div className="Comanda">
+            <ComandaHeader />
+            <div className = "content"></div>
+            <ComandaFooter />
+          </div>
+        );
+    }
+}
+
+export default App;
