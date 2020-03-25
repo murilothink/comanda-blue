a100dd557faa8f1f533de9db2de70c019926fed0
Renato Vinicius dos Santos <renatods@ciandt.com>
2020-03-25 10:47:48 -0300

    Commit layout 1.0

---
+++ prototipo-ui/layout/ComandaFooter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './base.css';
+
+export default class ComandaFooter extends Component {
+    render(){
+        return(
+            <footer className="footer">
+                <h1 className="textfooter">Estags GU BLUE</h1>
+                <h3 className="textcopy">Copyright © Comanda - BLUE 2020.</h3>
+            </footer>
+        )
+    }
+}
