3781b63bf9dd32963ddd84f287140e0098e666e6
João Pedro Benicio Carnieri <jbenicio@ciandt.com>
2020-03-25 10:04:39 -0300

    comanda 1.0

---
+++ prototipo-ui/prototipo/comanda/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Comanda from './Comanda';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Comanda/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
