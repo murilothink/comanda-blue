2e91818e82927fe0cfbeb2b653661280b97d3403
lucianohrcit <lucianohr@ciandt.com>
2020-03-25 14:31:12 -0300

    Layout 1.1 fixed

--- prototipo-ui/prototipo/src/layout/App.js
+++ prototipo-ui/prototipo/src/layout/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import './base.css';
 import ComandaHeader from './ComandaHeader';
 import ComandaFooter from './ComandaFooter';
+import Menu from '../menu/Menu';
 
 class App extends Component {
     render(){
         return (
           <div className="Comanda">
             <ComandaHeader />
-            <div className = "content"></div>
+            <div className = "content">
+              <Menu />
+            </div>
             <ComandaFooter />
           </div>
         );
