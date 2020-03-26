ca380a1d18bcffe36f36343e5a58b472848c1e7c
Tiago Tasselli <tiagotasselli@ciandt.com>
2020-03-26 11:49:22 -0300

    Enum estatus comanda

--- webapp/src/main/java/br/com/nextgen2020/comandablue/model/enums/StatusComanda.java
+++ webapp/src/main/java/br/com/nextgen2020/comandablue/model/enums/StatusComanda.java
@@ -2,6 +2,23 @@ package br.com.nextgen2020.comandablue.model.enums;
 
 public enum StatusComanda {
 
-    ABERTO(),
-    FECHADO();
+    ABERTO("A", "Comanda Aberta"),
+    FECHADO("F", "Comanda Fechada");
+
+    StatusComanda(String codigoStatusMesa, String descricao) {
+        this.codigoStatusMesa = codigoStatusMesa;
+        this.descricao = descricao;
+    }
+
+    private String codigoStatusMesa;
+
+    private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String getCodigoStatusMesa() {
+        return codigoStatusMesa;
+    }
 }
