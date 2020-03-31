8c74b827060ebff694ca8d00782a324b59b32f8b
Renato Vinicius dos Santos <renatods@ciandt.com>
2020-03-31 11:19:19 -0300

    Menu combobox (devolvendo id selecionado)

--- comanda-frontend/src/pages/Menu/index.js
+++ comanda-frontend/src/pages/Menu/index.js
@@ -5,89 +5,98 @@ import MenuList from './menu_lista/MenuList';
 import ItemList from './item_lista/TabelaItems';
 import api from "../../services/api";
 
-export default function Menu(){
-    
-    const [state, setState] = React.useState({
-        carrinho: [],
-        categorias: [],
-        estabelecimento: {
-            id: 1,
-            nome: null
-        }
-    });
+export default class Menu extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            carrinho: [],
+            categorias: [],
+            estabelecimento: {
+                id: 1,
+                nome: null
+            },
+            idCategoriaAtual: -1,
+        };
+    }
 
 
     //Entra em loop componentDidMount componentDidUpdate
-    useEffect(()=> {
-        const url = "/estabelecimento/"+state.estabelecimento.id+"/categorias"
+    componentDidMount(){
+        const url = "/estabelecimento/"+this.state.estabelecimento.id+"/categorias"
         
-        if(!state.estabelecimento.nome){
-            api.get(url)
-            .then(response => {
-                setState({
-                    ... state,
-                    categorias: response.data.categorias,
-                    estabelecimento: response.data.estabelecimento
-                });  
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        }
-    });
+        api.get(url)
+        .then(response => {
+            this.setState({
+                ... this.state,
+                categorias: response.data.categorias,
+                estabelecimento: response.data.estabelecimento
+            });  
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    
+    }
 
     //todo
-    const handleChange = event => {
-        const idCategoriaAtual = event.target.idCategoriaAtual;
-        setState({
-          ...state,
-          [idCategoriaAtual]: event.target.value,
+    handleChange = event => {
+        this.setState({
+          ...this.state,
+          idCategoriaAtual: event.target.value,
         });
     };
 
     //adicione produto no state carrinho
-    const handleAddItem = (produto) =>{
-        let newCarrinho = state.carrinho.slice();
+    handleAddItem = (produto) =>{
+        let newCarrinho = this.state.carrinho.slice();
         newCarrinho.push(produto)
-        setState({
-            ... state,
+        this.setState({
+            ...this.state,
             carrinho: newCarrinho
         })
-        
+               
     }
 
-    return (
-        <div class="wrapper">
-            <div class="main_collumn">
-                <h1 class="estabelecimento_title">
-                    {state.estabelecimento.nome}
-                </h1>
-                <div class="row_1">
-                
-                <FormControl variant="outlined" className="ComboBox">
-                    <InputLabel>Categorias</InputLabel>
-                    <Select native label="Categoria"
-                    value={state.idCategoriaAtual}
-                    onChange={handleChange}>
-                        <option aria-label="None" value="" />
-                        {state.categorias.map((item, key) => {
-                            return(
-                            <option value={item.id}>{item.categoria}</option>
-                            )
-                        })}
-                    </Select>
-                    <FormHelperText>Selecione uma categoria</FormHelperText>
-                </FormControl>
-                
+    renderMenuList(){
+        alert(this.state.idCategoriaAtual)
+        return(
+            <MenuList OnAddItem={this.handleAddItem} idCategoria={this.state.idCategoriaAtual} idEstabelecimento={this.state.estabelecimento.id} />
+        )
+    }
 
-                <Button size="small" className="btn_extrato" variant="contained" color="primary">Exibir Extrato</Button>
-                </div>  
-                <MenuList OnAddItem={handleAddItem} idCategoria={-1} idEstabelecimento={1}/> 
-                
+    render(){
+        return (
+            <div class="wrapper">
+                <div class="main_collumn">
+                    <h1 class="estabelecimento_title">
+                        {this.state.estabelecimento.nome}
+                    </h1>
+                    <div class="row_1">
+                    
+                    <FormControl variant="outlined" className="ComboBox">
+                        <InputLabel>Categorias</InputLabel>
+                        <Select native label="Categoria"
+                        value={this.state.idCategoriaAtual}
+                        onChange={this.handleChange}>
+                            <option aria-label="None" value={-1} />
+                            {this.state.categorias.map((item, key) => {
+                                return(
+                                <option value={item.id}>{item.categoria}</option>
+                                )
+                            })}
+                        </Select>
+                        <FormHelperText>Selecione uma categoria</FormHelperText>
+                    </FormControl>
+                    
+                    <Button size="small" className="btn_extrato" variant="contained" color="primary">Exibir Extrato</Button>
+                    </div>  
+                    {this.renderMenuList()} 
+                    
+                </div>
+                <aside class="lista_itens_wrapper">
+                        {<ItemList carrinho={this.state.carrinho} />}
+                </aside>
             </div>
-            <aside class="lista_itens_wrapper">
-                    {<ItemList carrinho={state.carrinho} />}
-            </aside>
-        </div>
-    );
+        );
+    }
 }
