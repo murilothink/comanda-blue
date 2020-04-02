18c3afae6ae8973ec8dcd7443be33b80c8d6f20c
Renato Vinicius dos Santos <renatods@ciandt.com>
2020-04-02 13:25:24 -0300

    Tela Extrato integrada

--- webapp/src/main/js/src/Pages/TelaExtrato.js
+++ webapp/src/main/js/src/Pages/TelaExtrato.js
@@ -15,6 +15,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+
 // Para utilizar select
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
