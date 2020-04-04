7d273e24523bc35df802f8ae68db3445e9ba2039
lassal <lucalves33@yahoo.com.br>
2020-04-04 18:50:19 -0300

    Adicionado resourceHandlers para o ConfigCrossOrigin para prover recursos estáticos (imagens, arquivos) na pasta static. NOTA ao adicionar o WebMvcConfigurer esta configuração padrão se perde.
    Adicionado devDependencies e script de webpack para package.json na pasta js.
    
    Copiado pasta de imagens para pasta static/img e criado configuração no webpack para mapear estas imagens como URL da pasta static.
    
    Configurado webpack para processar style.css e os outros arquivos.
    
    Ajustado index.html para funcionar adequadamente com o arquivo gerado pelo webpack

--- webapp/src/main/java/br/com/nextgen2020/comandablue/ConfigCrossOrigin.java
+++ webapp/src/main/java/br/com/nextgen2020/comandablue/ConfigCrossOrigin.java
@@ -3,11 +3,22 @@ package br.com.nextgen2020.comandablue;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 public class ConfigCrossOrigin implements WebMvcConfigurer {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -7,7 +7,7 @@ var path = require('path');
 module.exports = {
 
     // Where to look for react application
-    entry: './src/main/js/src/index.js',
+    entry: ['./src/main/js/src/index.js'],
 
     // This option controls if and how source maps are generated
     // Creates sourcemaps so that, when you are debugging JS code in the browser,
@@ -19,7 +19,7 @@ module.exports = {
 
     // Providing the mode configuration option tells webpack to use its built-in optimizations accordingly.
     // 'development': Sets process.env.NODE_ENV on DefinePlugin to value development. Enables NamedChunksPlugin and NamedModulesPlugin
-    mode: 'development',
+    mode: 'production',
 
     // The top-level output key contains set of options instructing webpack on how and where it
     // should output your bundles, assets and anything else you bundle or load with webpack.
@@ -34,14 +34,37 @@ module.exports = {
         rules: [
             {
                 test: path.join(__dirname, '.'),
-                exclude: /(node_modules)/,
+                exclude: [/(node_modules)/, /\.(png|jpe?g|gif)$/i],
                 use: [{
                     loader: 'babel-loader',
                     options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"]
-                    }
+                        presets: ["@babel/preset-env"
+                                , "@babel/preset-react"
+                                ],
+                        plugins: [ "@babel/plugin-proposal-class-properties"
+                                  ,["@babel/plugin-proposal-decorators", { "legacy": true }]
+                                  , "@babel/plugin-transform-runtime"
+                                 ]
+                    },
                 }]
-            }
+            },
+            {
+                    test: /\.(png|jpe?g|gif)$/i,
+                    use: [
+                      {
+                        loader: 'file-loader',
+                        options: {
+                           name: '[name].[ext]',
+                           publicPath: 'img',
+                           emitFile: false
+                        }
+                      },
+                    ],
+            },
+            {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+            },
         ]
 
     }
